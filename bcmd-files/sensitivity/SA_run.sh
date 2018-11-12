fdca166de85c467151d313064b304cb387594113
Josh Buckland <buck06191@gmail.com>
2018-11-12 17:32:47 +0000

    Initial hypothermia files

---
+++ bcmd-files/sensitivity/SA_run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+DATASET="LWP475"
+OUTDIR="../../data/SA_results/${DATASET}"
+mkdir -p ${OUTDIR}
+python ~/repos/Github/BayesCMD/batch/dsim.py -o ${OUTDIR} hypothermia_SA.dsimjob -b ~/repos/Github/BayesCMD/build ../../data/clean_hypothermia/cleaned_${DATASET}_filtered.csv 
