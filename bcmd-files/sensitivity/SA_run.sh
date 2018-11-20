3accf62548f0460c56243ccef1c33573c77ebf03
Josh Buckland <buck06191@gmail.com>
2018-11-20 14:23:02 +0000

    fix bug in storage of sensitivity output

--- bcmd-files/sensitivity/SA_run.sh
+++ bcmd-files/sensitivity/SA_run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DATASET="LWP484"
-OUTDIR="../../data/SA_results/${DATASET}"
+OUTDIR=`readlink -m "../../data/SA_results/${DATASET}"`
+echo "Writing to ${OUTDIR}"
 mkdir -p ${OUTDIR}
 python ~/repos/Github/BayesCMD/batch/dsim.py -o ${OUTDIR} hypothermia_SA.dsimjob -b ~/repos/Github/BayesCMD/build ../../data/clean_hypothermia/cleaned_${DATASET}_filtered.csv 
