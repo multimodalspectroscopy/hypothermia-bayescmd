a81cf24a2f332684d736fa5b084e0c75d764736b
Josh Buckland <buck06191@gmail.com>
2018-11-27 11:58:24 +0000

    Add test script to fit ABC on LWP475

---
+++ config_files/abc/myriad_lwp475.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+#$ -l h_rt=1:30:00
+#$ -N LWP475_cellDeath
+#$ -wd /home/ucbpjru/Scratch
+# Set up the job array.  In this instance we have requested 1000 tasks
+# numbered 1 to 1000.
+#$ -t 1-10
+
+module load python3/recommended
+cd $TMPDIR
+export BASEDIR="$HOME/BayesCMD"
+
+DATASET="LWP475"
+OUTDIR=`readlink -m "../../data/ABC_results/with_cellDeath/${DATASET}"`
+DATADIR=`readink -m "../../data/clean_hypothermia/${DATASET}_filtered_formatted.csv"`
+CONFIGFILE=`readlink -m "./bp_hypothermia_config.json"`
+
+echo "# Creating out directory ${OUTDIR}"
+mkdir -p ${OUTDIR}
+
+start=`date +%s`
+python3 $BASEDIR/scripts/batch.py 1000 $DATAFILE $CONFIGFILE --workdir $TMPDIR
+echo "Duration: $(($(date +%s)-$start))" > $TMPDIR/$SGE_TASK_ID.timings.txt
+
+tar -zcvf $HOME/Scratch/batch_$JOB_NAME.$SGE_TASK_ID.tar.gz $TMPDIR
