1df68b27da18f0b1630fc9b83144f135790537d1
Joshua Russell-Buckland <ucbpjru@login13.myriad.ucl.ac.uk>
2018-12-06 13:24:20 +0000

    myriad output files placed in dataset directory to make downloading easier

--- config_files/abc/myriad_lwp481.sh
+++ config_files/abc/myriad_lwp481.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #$ -l h_rt=1:30:00
-#$ -N LWP475_cellDeath
+#$ -N LWP481_cellDeath
 #$ -wd /home/ucbpjru/Scratch
 # Set up the job array.  In this instance we have requested 1000 tasks
 # numbered 1 to 1000.
@@ -10,11 +10,11 @@ module load python3/recommended
 cd $TMPDIR
 export BASEDIR="${HOME}/BayesCMD"
 
-DATASET="LWP475"
+DATASET="LWP481"
 DATAFILE="${HOME}/hypothermia-bayescmd/data/clean_hypothermia/${DATASET}_filtered_formatted.csv"
 CONFIGFILE="${HOME}/hypothermia-bayescmd/config_files/abc/bp_hypothermia_config.json"
 
-echo "Datafile is ${DATFILE}\nConfig file is ${CONFIGFILE}."
+echo -e "Datafile is ${DATFILE}\nConfig file is ${CONFIGFILE}."
 
 start=`date +%s`
 python3 $BASEDIR/scripts/batch.py 1000 $DATAFILE $CONFIGFILE --workdir $TMPDIR
--- config_files/abc/myriad_lwp484.sh
+++ config_files/abc/myriad_lwp484.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #$ -l h_rt=1:30:00
-#$ -N LWP475_cellDeath
+#$ -N LWP484_cellDeath
 #$ -wd /home/ucbpjru/Scratch
 # Set up the job array.  In this instance we have requested 1000 tasks
 # numbered 1 to 1000.
@@ -10,15 +10,14 @@ module load python3/recommended
 cd $TMPDIR
 export BASEDIR="${HOME}/BayesCMD"
 
-DATASET="LWP475"
+DATASET="LWP484"
 DATAFILE="${HOME}/hypothermia-bayescmd/data/clean_hypothermia/${DATASET}_filtered_formatted.csv"
 CONFIGFILE="${HOME}/hypothermia-bayescmd/config_files/abc/bp_hypothermia_config.json"
 
-echo "Datafile is ${DATFILE}\nConfig file is ${CONFIGFILE}."
+echo -e "Datafile is ${DATFILE}\nConfig file is ${CONFIGFILE}."
 
 start=`date +%s`
 python3 $BASEDIR/scripts/batch.py 1000 $DATAFILE $CONFIGFILE --workdir $TMPDIR
 echo "Duration: $(($(date +%s)-$start))" > $TMPDIR/$SGE_TASK_ID.timings.txt
 
-tar -zcvf $HOME/Scratch/${DATASET}/batch_$JOB_NAME.$SGE_TASK_ID.tar.gz 
-$TMPDIR
+tar -zcvf $HOME/Scratch/${DATASET}/batch_$JOB_NAME.$SGE_TASK_ID.tar.gz $TMPDIR
