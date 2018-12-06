f341d985229356ed0ebd1e5efdd6889ce7742405
Josh Buckland <buck06191@gmail.com>
2018-12-06 13:56:29 +0000

    Automated sensitivity across model versions

---
+++ bcmd-files/sensitivity/LWP476.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Call each model in turn
+
+for i in {1..3};
+do
+    /home/buck06191/repos/Github/hypothermia-bayescmd/bcmd-files/sensitivity/bp_hypothermia_${i}/SA_LWP475.sh "/home/buck06191/repos/Github/hypothermia-bayescmd/bcmd-files/sensitivity/bp_hypothermia_${i}/"
+done
--- bcmd-files/sensitivity/bp_hypothermia_2/SA_LWP479.sh
+++ bcmd-files/sensitivity/bp_hypothermia_2/SA_LWP479.sh
@@ -3,8 +3,11 @@
 DATASET="LWP479"
 OUTDIR1=`readlink -m "../../data/SA_results/bph_2/with_cellDeath/nrmse/${DATASET}"`
 OUTDIR2=`readlink -m "../../data/SA_results/bph_2/with_cellDeath/nrmse_zero/${DATASET}"`
-JOBFILE1=hypothermia_SA.dsimjob
-JOBFILE2=hypothermia_SA_zero.dsimjob
+
+WORKDIR=$1
+JOBFILE1="${WORKDIR}hypothermia_SA.dsimjob"
+JOBFILE2="${WORKDIR}hypothermia_SA_zero.dsimjob"
+
 DATAFILE="../../data/clean_hypothermia/${DATASET}_filtered_formatted.csv"
 
 echo "Writing to ${OUTDIR1}"
