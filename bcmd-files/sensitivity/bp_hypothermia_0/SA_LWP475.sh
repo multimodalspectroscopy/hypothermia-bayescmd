dd44278a234c8cbe6a3d87f18ab06bb4ae025fd7
Josh Buckland <buck06191@gmail.com>
2019-05-03 10:24:53 +0100

    BPH0 scripts

---
+++ bcmd-files/sensitivity/bp_hypothermia_0/SA_LWP475.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DATASET="LWP475"
+OUTDIR1=`readlink -m "../../data/SA_results/bph_0/with_cellDeath/nrmse/${DATASET}"`
+OUTDIR2=`readlink -m "../../data/SA_results/bph_0/with_cellDeath/nrmse_zero/${DATASET}"`
+
+WORKDIR=$1
+JOBFILE1="${WORKDIR}hypothermia_SA.dsimjob"
+JOBFILE2="${WORKDIR}hypothermia_SA_zero.dsimjob"
+
+DATAFILE="../../data/clean_hypothermia/${DATASET}_filtered_formatted.csv"
+
+echo "Writing to ${OUTDIR1}"
+mkdir -p ${OUTDIR1}
+mkdir -p ${OUTDIR2}
+python ~/repos/Github/BayesCMD/batch/dsim.py -o ${OUTDIR1} -b ~/repos/Github/BayesCMD/build ${JOBFILE1} ${DATAFILE}
+
+echo "Writing to ${OUTDIR2}"
+mkdir -p ${OUTDIR2}
+mkdir -p ${OUTDIR2}
+python ~/repos/Github/BayesCMD/batch/dsim.py -o ${OUTDIR2} -b ~/repos/Github/BayesCMD/build ${JOBFILE2} ${DATAFILE}
+
