ef1c0f084343db98c7660011bc3320ba7e9411b6
Josh Buckland <buck06191@gmail.com>
2019-06-26 11:31:06 +0100

    Add *.1 model configs

---
+++ config_files/abc/bp_hypothermia_1_1/myriad_lwp475_1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+#$ -l h_rt=1:30:00
+#$ -N LWP475_cellDeath_1
+#$ -o /home/ucbpjru/Scratch/bph1/LWP475/out
+#$ -e /home/ucbpjru/Scratch/bph1/LWP475/err
+#$ -wd /home/ucbpjru/Scratch
+# Set up the job array.  In this instance we have requested 1000 tasks
+# numbered 1 to 1000.
+#$ -t 1-50000
+
+module load python3/recommended
+cd $TMPDIR
+export BASEDIR="${HOME}/BayesCMD"
+
+DATASET="LWP475"
+DATAFILE="${HOME}/hypothermia-bayescmd/data/clean_hypothermia/${DATASET}_filtered_formatted.csv"
+CONFIGFILE="${HOME}/hypothermia-bayescmd/config_files/abc/bp_hypothermia_1/bp_hypothermia_1_config.json"
+
+echo "Datafile is ${DATAFILE}\nConfig file is ${CONFIGFILE}."
+
+start=`date +%s`
+python3 $BASEDIR/scripts/batch.py 1000 $DATAFILE $CONFIGFILE --workdir $TMPDIR
+echo "Duration: $(($(date +%s)-$start))" > $TMPDIR/$SGE_TASK_ID.timings.txt
+
+tar -zcvf $HOME/Scratch/bph1/${DATASET}/batch_$JOB_NAME.$SGE_TASK_ID.tar.gz $TMPDIR
